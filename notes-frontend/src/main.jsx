5a52d49dbfddcf2591a283b88ccffce63365f948
murilozgodoy <murilozg@al.insper.edu.br>
2025-11-03 20:13:00 -0300

    fix: implementacao da logo

--- notes-frontend/src/main.jsx
+++ notes-frontend/src/main.jsx
@@ -2,6 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import logoEstudai from './assets/logo_estudai.jpg'
+
+const setFavicon = (url) => {
+  let link = document.querySelector("link[rel~='icon']")
+  if (!link) {
+    link = document.createElement('link')
+    link.rel = 'icon'
+    document.getElementsByTagName('head')[0].appendChild(link)
+  }
+  link.href = url
+}
+
+setFavicon(logoEstudai)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
